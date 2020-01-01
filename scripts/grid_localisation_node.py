d1d82cdf3cf205804c4384c93f3faf4e3f4d2c8d
Khaled <khaledsoultan63@gmail.com>
2020-01-01 17:04:43 +0000

    Implemented line 2of table8.1

--- scripts/grid_localisation_node.py
+++ scripts/grid_localisation_node.py
@@ -83,20 +83,32 @@ def init():
     map_height = map.info.resolution*map.info.height
 
     linear_resolution = 0.15 # 15 cm
-    angular_resolution = 5 # degrees
+    angular_resolution = math.radians(5) # degrees to radians
 
     grid_width = math.floor(map_width/linear_resolution)
     grid_length = math.floor(map_height/linear_resolution)
-    grid_depth = math.floor(360/angular_resolution)
+    grid_depth = math.floor((2*math.pi))/angular_resolution)
     number_of_cells_in_grid = grid_width * grid_length * grid_depth
 
     # Initialise distributions uniformly
     previous_dist = np.full([grid_width, grid_length, grid_depth], 1/number_of_cells_in_grid)
     current_dist = previous_dist
 
+    pbarkt = np.zeros([grid_width, grid_length, grid_depth])
+
     while not rospy.is_shutdown():
         for k in range(number_of_cells_in_grid): # line 2 of table 8.1
-            
+            [rowk, colk, depthk] = np.unravel_index(k,[grid_width, grid_length, grid_depth])
+            xt = [rowk, colk, depthk*angular_resolution]
+
+            # =============== Prediction ================
+            for i in range(number_of_cells_in_grid):
+                [rowi, coli, depthi] = np.unravel_index(i, [grid_width, grid_length, grid_depth])
+                xt_d1 = [rowi, coli, depthi*angular_resolution]
+
+                pbarkt[rowk, colk, depthk] += previous_dist[rowi, coli, depthi]*motion_model(xt, ut, xt_d1)
+            # ===========================================
+
         prior = motion_model(current_pose, [previous_odata, current_odata], previous_pose)
 
 
