822fa3e4da86d4c6c032adf47ace06424f7d4cb4
Khaled <khaledsoultan63@gmail.com>
2020-01-02 12:55:49 +0000

    Added grid resolution

--- src/grid_localisation_node.cpp
+++ src/grid_localisation_node.cpp
@@ -1,6 +1,8 @@
 #include "ros/ros.h"
 #include "nav_msgs/Odometry.h"
+#include "nav_msgs/OccupancyGrid.h"
 #include "nav_msgs/GetMap.h"
+#include <cmath>
 
 // Define global variables
 nav_msgs::Odometry current_odom;
@@ -26,6 +28,16 @@ int main(int argc, char **argv)
   else
     ROS_INFO("Failed to call map service");
 
+  // Define grid resolution
+  float map_width = map_srv.response.map.info.resolution*map_srv.response.map.info.width;
+  float map_height = map_srv.response.map.info.resolution*map_srv.response.map.info.height;
+
+  float linear_resolution = 0.15; // 15 cm
+  float angular_resolution = 0.0873; // 5 degress in radians
+
+  int grid_width = std::floor(map_width/linear_resolution);
+  int grid_length = std::floor(map_height/linear_resolution);
+  int grid_depth = std::floor((2*M_PI)/angular_resolution);
   /**
    * ros::spin() will enter a loop, pumping callbacks.  With this version, all
    * callbacks will be called from within this thread (the main one).  ros::spin()
