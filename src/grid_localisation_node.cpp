e19063c967e888b87b1a3f74f66b8537d998d000
Khaled <khaledsoultan63@gmail.com>
2020-01-02 16:11:05 +0000

    Implemented for loops for motion_model step

--- src/grid_localisation_node.cpp
+++ src/grid_localisation_node.cpp
@@ -6,10 +6,31 @@
 
 // Define global variables
 nav_msgs::Odometry current_odom;
+nav_msgs::Odometry previous_odom;
 
 void odomCallback(const nav_msgs::Odometry::ConstPtr& msg)
 {
   current_odom.pose.pose = msg->pose.pose;
+}
+
+int* ind2sub(int index, int N1, int N2, int N3)
+{
+  // Don't event ask
+  // https://eli.thegreenplace.net/2015/memory-layout-of-multi-dimensional-arrays/
+  static int sub[3];
+  int n1, n2, n3;
+  n3 = index % N3;
+  n2 = ((index-n3)/N3)%N2;
+  n1 = (((index-n3)/N3)-n2)/N2;
+  sub[0] = n1;
+  sub[1] = n2;
+  sub[2] = n3;
+  return sub;
+}
+
+bool isNoMovement(nav_msgs::Odometry prev, nav_msgs::Odometry curr)
+{
+  float linear_tol=0.1, angular_tol=
 }
 
 int main(int argc, char **argv)
@@ -38,6 +59,39 @@ int main(int argc, char **argv)
   int grid_width = std::floor(map_width/linear_resolution);
   int grid_length = std::floor(map_height/linear_resolution);
   int grid_depth = std::floor((2*M_PI)/angular_resolution);
+
+  long number_of_grid_cells = grid_width*grid_length*grid_depth;
+
+  // Initialise distribution uniformly
+  std::vector<std::vector<std::vector<float> > > previous_dist (grid_width,std::vector<std::vector<float> >(grid_length,std::vector <float>(grid_depth,1/number_of_grid_cells)));
+  std::vector<std::vector<std::vector<float> > > current_dist;
+  current_dist = previous_dist;
+
+  // Initliase with zeros
+  std::vector<std::vector<std::vector<float> > > p_bar_kt (grid_width,std::vector<std::vector<float> >(grid_length,std::vector <float>(grid_depth,0)));
+
+  previous_odom = current_odom;
+
+  ros::Rate loop_rate(10); // 10 Hz
+  while(ros::ok())
+  {
+    if(isNoMovement(previous_odom, current_odom)) // Don't do anything if the robot didn't move
+      continue;
+
+    for(long k = 0; k < number_of_grid_cells; k++)
+    {
+      for(long i = 0; i < number_of_grid_cells; i++)
+      {
+        int* sub = ind2sub(k, grid_width, grid_length, grid_depth);
+        int rowk = sub[0], colk = sub[1], depthk = sub[2];
+
+      }
+    }
+
+    ros::spinOnce();
+    loop_rate.sleep();
+  }
+
   /**
    * ros::spin() will enter a loop, pumping callbacks.  With this version, all
    * callbacks will be called from within this thread (the main one).  ros::spin()
