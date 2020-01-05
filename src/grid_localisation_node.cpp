ad466e532c146d562acbd863aa97ae523e784780
Khaled <khaledsoultan63@gmail.com>
2020-01-05 19:19:27 +0000

    implemented the motion model

--- src/grid_localisation_node.cpp
+++ src/grid_localisation_node.cpp
@@ -66,6 +66,65 @@ bool isNoMovement(tf::StampedTransform prev, tf::StampedTransform curr)
   return false;
 }
 
+double normalise(double z)
+{
+  return atan2(sin(z),cos(z));
+}
+
+double angle_diff(double a, double b)
+{
+  double d1, d2;
+  a = normalise(a);
+  b = normalise(b);
+  d1 = a-b;
+  d2 = 2*M_PI - fabs(d1);
+  if(d1 > 0)
+    d2 *= -1.0;
+  if(fabs(d1) < fabs(d2))
+    return(d1);
+  else
+    return(d2);
+}
+
+double prob(double a, double b)
+{
+  return (1/sqrt(2*M_PI*b))*exp(-0.5*(pow(a, 2)/b));
+}
+
+double motion_model(double* xt, tf::StampedTransform prev, tf::StampedTransform curr, double* xt_d1)
+{
+  float alpha1 = 0.2, alpha2 = 0.2, alpha3=0.8, alpha4=0.2, alpha5=0.1;
+  double x_prime = xt[0];
+  double y_prime = xt[1];
+  double theta_prime = xt[2];
+
+  double x_bar = prev.getOrigin().getX();
+  double y_bar = prev.getOrigin().getY();
+  double theta_bar = tf::getYaw(prev.getRotation());
+
+  double x_bar_prime = curr.getOrigin().getX();
+  double y_bar_prime = curr.getOrigin().getY();
+  double theta_bar_prime = tf::getYaw(curr.getRotation());
+
+  double x = xt_d1[0];
+  double y = xt_d1[1];
+  double theta = xt_d1[2];
+
+  double delta_rot1 = angle_diff(atan2(y_bar_prime-y_bar, x_bar_prime-x_bar), theta_bar);
+  double delta_trans = sqrt(pow((x_bar - x_bar_prime), 2) + pow((y_bar - y_bar_prime), 2));
+  double delta_rot2 = angle_diff(theta_bar_prime, angle_diff(theta_bar, delta_rot1)); // NOTE: for some reason AMCL doesn't subtract the second theta
+
+  double delta_rot1_hat = angle_diff(atan2(y_prime-y, x_prime-x), theta);
+  double delta_trans_hat = sqrt(pow((x-x_prime), 2) + pow((y-y_prime), 2));
+  double delta_rot2_hat = angle_diff(theta_prime, angle_diff(theta, delta_rot1_hat));
+
+  double p1 = prob(angle_diff(delta_rot1, delta_rot1_hat), alpha1*(pow(delta_rot1_hat,2))+alpha2*(pow(delta_trans, 2)));
+  double p2 = prob(delta_trans-delta_trans_hat, alpha3*(pow(delta_trans_hat, 2))+alpha4*(pow(delta_rot1_hat, 2))+alpha4*(pow(delta_rot2_hat, 2)));
+  double p3 = prob(angle_diff(delta_rot2, delta_rot2_hat), alpha1*(pow(delta_rot2_hat, 2))+alpha2*(pow(delta_trans_hat, 2)));
+
+  return p1*p2*p3;
+}
+
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "grid_localisation");
@@ -127,13 +186,26 @@ int main(int argc, char **argv)
     for(long k = 0; k < number_of_grid_cells; k++) // line 2 of table 8.1
     {
       int rowk, colk, depthk;
-      ind2sub(k, grid_width, grid_length, grid_depth, &rowk, &colk, &depthk);
+      double xt[3];
 
+      ind2sub(k, grid_width, grid_length, grid_depth, &rowk, &colk, &depthk);
+      xt[0] = rowk*linear_resolution;
+      xt[1] = colk*linear_resolution;
+      xt[2] = depthk*angular_resolution;
+
+      // TODO: BE CAREFUL ABOUT WHEN YOU TAKE THE TRANSFORM READINGS
       // ================ Prediction =================
       for(long i = 0; i < number_of_grid_cells; i++)
       {
         int rowi, coli, depthi;
+        double xt_d1[3];
+
         ind2sub(i, grid_width, grid_length, grid_depth, &rowi, &coli, &depthi);
+        xt_d1[0] = rowi*linear_resolution;
+        xt_d1[1] = coli*linear_resolution;
+        xt_d1[2] = depthi*angular_resolution;
+
+        p_bar_kt[rowk][colk][depthk] += previous_dist[rowi][coli][depthi]*motion_model(xt, previous_transform, current_transform, xt_d1);
       }
     }
     previous_transform = current_transform;
@@ -143,12 +215,7 @@ int main(int argc, char **argv)
     loop_rate.sleep();
   }
 
-  /**
-   * ros::spin() will enter a loop, pumping callbacks.  With this version, all
-   * callbacks will be called from within this thread (the main one).  ros::spin()
-   * will exit when Ctrl-C is pressed, or the node is shutdown by the master.
-   */
-  ros::spin();
+  ros::spin(); // do I need this?
 
   return 0;
 }
