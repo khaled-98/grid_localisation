45a3451b11e059a33f1c5a97d0af830a6ecb1952
Khaled <khaledsoultan63@gmail.com>
2020-02-10 09:32:46 +0000

    Rewritten angle_diff for simplification

--- src/grid_localisation_node.cpp
+++ src/grid_localisation_node.cpp
@@ -47,18 +47,7 @@ bool isNoMovement(tf::StampedTransform prev, tf::StampedTransform curr)
 
 double angle_diff(double a, double b)
 {
-  double d1, d2;
-  a = normalise(a);
-  b = normalise(b);
-  d1 = a-b;
-  d2 = 2*M_PI - fabs(d1);
-  if(d1 > 0)
-    d2 *= -1.0;
-  if(fabs(d1) < fabs(d2))
-    return(d1);
-  else
-    return(d2);
-}
+  double angle = a - b;
 
   angle = fmod(a, 2.0*M_PI); // limit the angle from 0 to 2*Pi
 
